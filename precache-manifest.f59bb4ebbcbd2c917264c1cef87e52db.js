6089299fd31a1b2073745ee907c913ebc9151b28
lukasz-fiuk <kopafiuk@gmail.com>
2020-10-14 17:24:51 +0200

    Updates

---
+++ precache-manifest.f59bb4ebbcbd2c917264c1cef87e52db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2ce5722c83551ecca325423eda713f",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "0aa183e5cd7172fd549f",
+    "url": "/react-movie-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4713afc9d8b52ed3ccf1",
+    "url": "/react-movie-search/static/js/2.b64f5e76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.b64f5e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa183e5cd7172fd549f",
+    "url": "/react-movie-search/static/js/main.e43668ba.chunk.js"
+  },
+  {
+    "revision": "ec1afc3891ded9c1372a",
+    "url": "/react-movie-search/static/js/runtime-main.33682eb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-movie-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
